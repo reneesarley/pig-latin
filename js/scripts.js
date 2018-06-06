a5446de3f0e8d89280e186ec1ab0191f6dfb6cfd
Reese Glasscock and Renee Sarley <reese+renee@>
2018-06-06 12:02:47 -0700

    update business logic for second for rule that would adjust letters that are longer than one character

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,57 @@
-$(function(){
-  console.log("test")
 
+var outPut = function(translatedPig){
+  $("span#output").text(translatedPig);
+};
+
+
+var pigLatinGen = function(pigLatinConversion){
+  var words = pigLatinConversion.split(" ");
+  var vowels =["a", "e", "i", "o", "u", "y"];
+
+  for(word = 0; word < words.length; word ++){
+    for(vowel = 0; vowel < vowels.length; vowel ++){
+      if(words[word] === vowels[vowel]){
+      words[word] = words[word] + "ay";
+      vowel = vowels.length;
+      } else if(words[word].length > 1){
+        console.log(words[word]);
+        var letters = (words[word]).split("");
+        for(letter = 0; letter < letters.length; letter ++){
+          console.log(letters[letter]);
+          for(vowel = 0; vowel < vowels.length; vowel ++){
+            if(letters[letter] !== vowels[vowel]){
+              letters.push(letters[letter]);
+              words[word] = toString(letters[letter]);
+              console.log("this is letters " + letters);
+              console.log("this is words " + words[word]);
+              letter = letters.length;
+
+            } else{
+              letter = letters.length;
+            }
+          }
+        }
+        words[word] = words[word] + "way";
+        vowel = vowels.length;
+        console.log("made it through:" + words[word]);
+        console.log("anything with two or more");
+        }
+    }
+  }
+  outPut(words);
+};
+
+
+
+
+
+$(function(){
+  $("#pigForm").submit(function(event){
+    event.preventDefault();
+    console.log("btn works");
+    var userString = $("input#userString").val();
+    pigLatinGen(userString);
+
+  });
 
 });
